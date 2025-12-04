833d20d6b0b87b02398ef33ca4955749c50fe7d4
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-12-04 15:01:03 -0300

    Update excluir_evento.php

--- excluir_evento.php
+++ excluir_evento.php
@@ -1,16 +1,16 @@
 <?php
     require_once "src/Database/Conecta.php";
-require_once "src/Helpers/Utils.php";
-require_once "src/Models/Eventos.php";
-require_once "src/Models/IntegranteEvento.php";
-require_once "src/Models/FotoEvento.php";
-require_once "src/Services/EventoServicos.php";
-require_once "src/Services/EstilosMusicaisServicos.php";
-require_once "src/Services/AutenticarServico.php";
-session_start();
-$eventoServico = new EventoServicos();
+    require_once "src/Helpers/Utils.php";
+    require_once "src/Models/Eventos.php";
+    require_once "src/Models/IntegranteEvento.php";
+    require_once "src/Models/FotoEvento.php";
+    require_once "src/Services/EventoServicos.php";
+    require_once "src/Services/EstilosMusicaisServicos.php";
+    require_once "src/Services/AutenticarServico.php";
+    session_start();
+    $eventoServico = new EventoServicos();
 
-$id = Utils::sanitzar($_GET['id'], 'inteiro');
+    $id = Utils::sanitzar($_GET['id'], 'inteiro');
 
-
+    if(!id) Utils::redirecionarPara('meus_eventos.php');
 ?>
