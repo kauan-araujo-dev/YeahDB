3093708d168b64705721759441941c99fbc94e95
Rodvehman <63985415+Rodvehman@users.noreply.github.com>
2025-12-04 14:55:57 -0300

    excluir evento

---
+++ excluir_evento.php
@@ -0,0 +1,16 @@
+<?php
+    require_once "src/Database/Conecta.php";
+require_once "src/Helpers/Utils.php";
+require_once "src/Models/Eventos.php";
+require_once "src/Models/IntegranteEvento.php";
+require_once "src/Models/FotoEvento.php";
+require_once "src/Services/EventoServicos.php";
+require_once "src/Services/EstilosMusicaisServicos.php";
+require_once "src/Services/AutenticarServico.php";
+session_start();
+$eventoServico = new EventoServicos();
+
+$id = Utils::sanitzar($_GET['id'], 'inteiro');
+
+
+?>
