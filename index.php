ed0fec63c0db13a34ad195b36592ecdf9d5236b7
isaxo-dev <isaxo123321@gmail.com>
2025-12-08 15:59:16 -0300

    imagens adicionadas

--- index.php
+++ index.php
@@ -49,7 +49,7 @@ $estilos_musicais = $estilosMusicaisServicos->buscarEstilosComLimite();
           </div>
         </div>
         <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
-          <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+          <span class="carousel-cosntrol-prev-icon" aria-hidden="true"></span>
           <span class="visually-hidden">Previous</span>
         </button>
         <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
